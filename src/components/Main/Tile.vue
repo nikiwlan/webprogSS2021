1f2daedfeca85236f685410218bf5a6db4871b3a
Niklas Langes <niklas.langes@hs-weingarten.de>
2021-06-16 23:19:56 +0200

    tile

--- src/components/Main/Tile.vue
+++ src/components/Main/Tile.vue
@@ -1,8 +1,11 @@
 <template>
     <div class= "tile">
         <div class = "tile-image">
-            <img src="../../../content/AlcFree.jpg" alt="Cocktail">
+
         </div>  
+        <div>
+            <img id = "alc-free"  src="../../../content/AlcFree.jpg" alt="Cocktail">
+        </div>
         <div class= "tile-body">
             <div class = "cocktail-name">
                 <h3>{{BezeichnungCocktail}} Name</h3>
@@ -53,6 +56,19 @@ export default {
     padding: 20px;
 }
 
+.tile-image{
+    background-image: url('../../../content/AlcFree.jpg') ;
+    background-repeat: no-repeat;
+    background-attachment:scroll;
+    background-size:cover;
+    padding: 20px;
+    padding-bottom: 10px;
+    width: 200px;
+    height: 230px;
+    margin: 20px;
+    border-radius: 20px;
+}
+
 cocktail-info{
     float: left;
     width: 200px;
@@ -60,8 +76,8 @@ cocktail-info{
     padding: 20px;
 }
 
-img{
-    width: 100%;
+#alc-free{
+    width: 40%;
 }
 
 </style>
