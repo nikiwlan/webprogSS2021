b2f5c222ee02b51f86ae65704e851a4a36c8c46a
Erik Cichy <erik.cichy@hs-weingarten.com>
2021-06-21 16:30:08 +0200

    prototype Filter

--- src/components/Main/TileMap.vue
+++ src/components/Main/TileMap.vue
@@ -1,7 +1,7 @@
 <template>
   <body class="view">
     <div class="heading">
-      <h2>Lass dich inspirieren {{categories}}</h2>
+      <h2>Lass dich inspirieren {{ categories }}</h2>
     </div>
     <div class="tile-map">
       <div v-for="cocktailID in cocktailList" :key="cocktailID">
@@ -23,13 +23,10 @@ export default {
   data() {
     return {
       cocktailList: [],
-      searchFieldList: [],
-      alcoholicList: [],
     };
   },
   props: ["categories", "searchField"],
   methods: {
-
     getAllCocktails() {
       console.log("Fetching All Cocktails...");
       this.cocktailList = [];
@@ -55,6 +52,61 @@ export default {
               response2.data.drinks[j].idDrink;
           }
         });
+    },
+
+    getCocktail(id) {
+      this.axios
+        .get("https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + id)
+        .then((response) => {
+          if (response.data.drinks != null) {
+            return response.data.drinks[0];
+          }
+          return null;
+        });
+    },
+
+    filterByAlc() {
+      // TODO
+      let tempList = [];
+      let index = 0;
+      let tempCocktail;
+      if (index == 0) {
+        for (let i; i < this.cocktailList.length; i++) {
+          tempCocktail = this.getCocktail(this.cocktailList[i]);
+          if (tempCocktail.strAlcoholic == "Alcoholic") {
+            tempList[index] = this.cocktailList[i];
+            index++;
+          }
+        }
+        this.cocktailList = tempList;
+      } else if (index == 1) {
+        for (let i; i < this.cocktailList.length; i++) {
+          tempCocktail = this.getCocktail(this.cocktailList[i]);
+          if (tempCocktail.strAlcoholic == "Non_Alcoholic") {
+            tempList[index] = this.cocktailList[i];
+            index++;
+          }
+        }
+        this.cocktailList = tempList;
+      }
+    },
+
+    filterByIng() {
+      let tempList = [];
+      let index = 0;
+      let tempCocktail;
+      if (this.categories != null) {
+        for (let i; i < this.cocktailList.length; i++) {
+          tempCocktail = this.getCocktail(this.cocktailList[i]);
+          for (let j; j < this.categories.length; j++) {
+            if (tempCocktail.strIngredient1 == this.categories[j]) {
+              tempList[index] = this.cocktailList[i];
+              index++;
+            }
+          }
+        }
+        this.cocktailList = tempList;
+      }
     },
   },
 
@@ -99,9 +151,8 @@ export default {
     }
   },
   watch: {
-    categories: function(){
-
-            console.log("asdfa");
+    categories: function () {
+      console.log("asdfa");
       console.log(this.categories[0]);
       // if (this.categorie.categorieValue) {
       //   console.log("123123123");
@@ -116,7 +167,6 @@ export default {
       //   for (let i = 0; i < response.data.drinks.length; i++) {
       //     this.categorie1[i] = response.data.drinks[i].strGlass;
       //   }
-
     },
 
     searchField: function () {
@@ -159,6 +209,8 @@ export default {
             }
           });
       }
+      this.filterByAlc();
+      this.filterByIng();
     },
   },
 };
