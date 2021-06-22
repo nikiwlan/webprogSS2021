bfdbc76730966c9bf317ed9c3c107012446de207
Erik Cichy <erik.cichy@hs-weingarten.com>
2021-06-22 15:55:44 +0200

    installed Vue Router

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import VueAxios from 'vue-axios';
 import axios from 'axios';
+import VueRouter from 'vue-router';
 
-createApp(App).use(VueAxios, axios).mount('#app')
+createApp(App).use(VueAxios, axios, VueRouter).mount('#app')
