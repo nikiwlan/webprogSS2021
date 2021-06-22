7ec686abbcdd362057487411d06d04f8163b09e0
Erik Cichy <erik.cichy@hs-weingarten.com>
2021-06-22 17:02:32 +0200

    kacke

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import VueAxios from 'vue-axios';
 import axios from 'axios';
-import router from "./router"
+import * as router from "./router"
 
 createApp(App).use(VueAxios, axios, router).mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,7 +15,7 @@ import Header from "../components/Header/Header.vue";
 import Main from "../components/Main/Main.vue";
 
 export default {
-  name: "App",
+  name: "Home",
   data() {
     return {
       searchField: "",
