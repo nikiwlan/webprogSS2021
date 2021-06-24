bf7fe9ab74fe5d3cbaac45ae12e625c3390f2832
Niklas Langes <niklas.langes@hs-weingarten.de>
2021-06-24 22:00:19 +0200

    hotfix

--- src/components/Main/TileMap.vue
+++ src/components/Main/TileMap.vue
@@ -56,79 +56,7 @@ export default {
         this.allCocktails.data.drinks[0].idDrink
     );
 
-<<<<<<< HEAD
     console.log(x.data);
-=======
-  beforeCreate: function () {
-    const getAlcoholic = async () => {
-      try {
-        return await this.axios
-          .get(
-            "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic"
-          )
-          .then((response) => {
-            return response.data.drinks;
-          });
-      } catch (err) {
-        console.error(err);
-      }
-    };
-    const getNonAlcoholic = async () => {
-      try {
-        return await this.axios
-          .get(
-            "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic"
-          )
-          .then((response) => {
-            return response.data.drinks;
-          });
-      } catch (err) {
-        console.error(err);
-      }
-    };
-    const getCocktailObject = async (id) => {
-      try {
-        return await this.axios
-          .get("https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + id)
-          .then((response) => {
-            return {
-              id: response.data.drinks[0].idDrink,
-              name: response.data.drinks[0].strDrink,
-              alc: response.data.drinks[0].strAlcoholic,
-              category: response.data.drinks[0].strCategory,
-            };
-          });
-      } catch (err) {
-        console.error(err);
-      }
-    };
-    const createArray = async () => {
-      try {
-        let alco = await getAlcoholic.apply();
-        let alcoFree = await getNonAlcoholic.apply();
-        let temp = [];
-        let cocktail;
-        console.log(alco[0]);
-        for (let i = 0; alco.length; i++) {
-          cocktail = await getCocktailObject.apply(null,[alco[i].idDrink]);
-          temp.push(cocktail);
-        }
-        for (let i = 0; alcoFree.length; i++) {
-          cocktail = await getCocktailObject.apply(null,[alcoFree[i].idDrink]);
-          temp.push(cocktail);
-        }
-        return temp;
-      } catch (err) {
-        console.error(err);
-      }
-    };
-    this.allCocktails = createArray.apply();
-  },
-  watch: {
-    categories: function () {
-      this.filterCocktails();
-    },
->>>>>>> 46abf719052c4de10ddc14fb2918c3e9e40e54e2
 
     console.log(this.allCocktails);
     console.log(this.allCocktails.data.drinks);
